84c6430535a05f224c8848f5d1ed398c7c448674
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-07 19:40:04 +0900

    다섯번째 테스트 통과

--- src/main/java/ch03/ExpiryDateCalculator.java
+++ src/main/java/ch03/ExpiryDateCalculator.java
@@ -1,6 +1,7 @@
 package ch03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
@@ -9,6 +10,10 @@ public class ExpiryDateCalculator {
         if (payData.getFirstBilldingDate() != null) {
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if(payData.getFirstBilldingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                if(YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBilldingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
+                }
+
                 return candidateExp.withDayOfMonth(payData.getFirstBilldingDate().getDayOfMonth());
             }
         }
