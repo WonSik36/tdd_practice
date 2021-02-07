e21e143ff664cc3cf2480c1b41936442ef759f96
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-07 18:11:15 +0900

    ExpiryDateCalculator.calculateExpiryDate 리팩토링 중

--- src/main/java/ch03/ExpiryDateCalculator.java
+++ src/main/java/ch03/ExpiryDateCalculator.java
@@ -4,6 +4,15 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return billingDate.plusMonths(1);
+        PayData data = PayData.builder()
+                .billingDate(billingDate)
+                .payAmount(payAmount)
+                .build();
+
+        return calculateExpiryDate(data);
+    }
+
+    public LocalDate calculateExpiryDate(PayData payData) {
+        return payData.getBillingDate().plusMonths(1);
     }
 }
