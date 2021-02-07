13ddcf1c4d17e53a14eff365677f538846266d50
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-07 19:54:46 +0900

    구현 코드 리팩토링

--- src/main/java/ch03/ExpiryDateCalculator.java
+++ src/main/java/ch03/ExpiryDateCalculator.java
@@ -8,16 +8,34 @@ public class ExpiryDateCalculator {
         int addedMonths = payData.getPayAmount() / 10_000;
 
         if (payData.getFirstBilldingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if(payData.getFirstBilldingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-                if(YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBilldingDate().getDayOfMonth()) {
-                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
-                }
-
-                return candidateExp.withDayOfMonth(payData.getFirstBilldingDate().getDayOfMonth());
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
+    }
 
-        return payData.getBillingDate().plusMonths(addedMonths);
+    private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+
+        if (!isSameDayOfMonth(payData.getFirstBilldingDate(), candidateExp)) {
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
+            final int dayOfFirstBilling = payData.getFirstBilldingDate().getDayOfMonth();
+
+            if (dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
+    }
+
+    private boolean isSameDayOfMonth(LocalDate date1, LocalDate date2) {
+        return date1.getDayOfMonth() == date2.getDayOfMonth();
+    }
+
+    private int lastDayOfMonth(LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
     }
 }
