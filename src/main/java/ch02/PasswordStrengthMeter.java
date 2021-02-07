853a109590e48e810148761998885033914e7d4f
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-07 16:07:52 +0900

    구현 코드 리팩토링

--- src/main/java/ch02/PasswordStrengthMeter.java
+++ src/main/java/ch02/PasswordStrengthMeter.java
@@ -6,22 +6,21 @@ public class PasswordStrengthMeter {
         if(s == null || s.isEmpty())
             return PasswordStrength.INVALID;
 
-        boolean lengthEnough = s.length() >= 8;
-        boolean containsNum = meetsContainingNumberCriteria(s);
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        int metCounts = 0;
 
-        if(lengthEnough && !containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        if(!lengthEnough && containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        if(!lengthEnough && !containsNum && containsUpp)
+        if(s.length() >= 8)
+            metCounts++;
+
+        if(meetsContainingNumberCriteria(s))
+            metCounts++;
+
+        if(meetsContainingUppercaseCriteria(s))
+            metCounts++;
+
+        if(metCounts == 1)
             return PasswordStrength.WEAK;
 
-        if(!lengthEnough)
-            return PasswordStrength.NORMAL;
-        if(!containsNum)
-            return PasswordStrength.NORMAL;
-        if(!containsUpp)
+        if(metCounts == 2)
             return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
