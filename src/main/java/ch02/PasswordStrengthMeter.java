30203115fb1ae694c1e993cbe26f79abc08fdd1e
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-07 15:51:38 +0900

    여섯번쨰(다섯번째) 테스트 통과

--- src/main/java/ch02/PasswordStrengthMeter.java
+++ src/main/java/ch02/PasswordStrengthMeter.java
@@ -14,12 +14,26 @@ public class PasswordStrengthMeter {
         if(!containsNum)
             return PasswordStrength.NORMAL;
 
+        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        if(!containsUpp)
+            return PasswordStrength.NORMAL;
+
         return PasswordStrength.STRONG;
     }
 
     private boolean meetsContainingNumberCriteria(String s) {
         for(char ch: s.toCharArray()) {
             if(ch >= '0' && ch<= '9') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean meetsContainingUppercaseCriteria(String s) {
+        for(char ch: s.toCharArray()) {
+            if(Character.isUpperCase(ch)){
                 return true;
             }
         }
