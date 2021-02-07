f819321ef9fb7cdea3badd69dbefb49f6dee35b7
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-07 14:55:48 +0900

    계산기 완성

--- src/main/java/ch02/Calculator.java
+++ src/main/java/ch02/Calculator.java
@@ -3,9 +3,6 @@ package ch02;
 public class Calculator {
 
     public static int plus(int n1, int n2) {
-        if(n1 == 4 && n2 == 1)
-            return 5;
-        else
-            return 3;
+        return n1 + n2;
     }
 }
