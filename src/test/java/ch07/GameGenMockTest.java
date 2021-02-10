20626b899ab1cabff0abba25fae96e648feabc37
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-10 11:58:35 +0900

    리턴 타입이 void인 메서드에 대해 예외 발생시키고 싶을시

--- src/test/java/ch07/GameGenMockTest.java
+++ src/test/java/ch07/GameGenMockTest.java
@@ -1,13 +1,19 @@
 package ch07;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.mock;
 
 public class GameGenMockTest {
+
+    @DisplayName("특정 값을 리턴하게 하려면")
     @Test
     void mockTest() {
         GameNumGen genMock = mock(GameNumGen.class);
@@ -17,6 +23,7 @@ public class GameGenMockTest {
         assertEquals("123", num);
     }
 
+    @DisplayName("익셉션을 발생시키려면")
     @Test
     void mockThrowTest() {
         GameNumGen genMock = mock(GameNumGen.class);
@@ -25,5 +32,16 @@ public class GameGenMockTest {
         assertThrows(IllegalArgumentException.class, () -> {
             genMock.generate(null);
         });
+    }
+
+    @DisplayName("리턴 타입이 void인 메서드에 대해 익셉션을 발생시키려면")
+    @Test
+    void voidMethodWillThrowTest() {
+        List<String> mockList = mock(List.class);
+        willThrow(UnsupportedOperationException.class)
+                .given(mockList)
+                .clear();
+
+        assertThrows(UnsupportedOperationException.class, () -> mockList.clear() );
     }
 }
