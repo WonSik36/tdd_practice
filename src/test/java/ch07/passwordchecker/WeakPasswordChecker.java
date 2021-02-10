b16948d3fff5428d8481d29e9fbb49620034dff2
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-10 09:58:03 +0900

    테스트 통과: 스텁 사용(WeakPasswordChecker)

--- src/test/java/ch07/passwordchecker/WeakPasswordChecker.java
+++ src/test/java/ch07/passwordchecker/WeakPasswordChecker.java
@@ -2,4 +2,5 @@ package ch07.passwordchecker;
 
 public interface WeakPasswordChecker {
 
+    boolean checkPasswordWeak(String pw);
 }
--- src/test/java/ch07/register/UserRegister.java
+++ src/test/java/ch07/register/UserRegister.java
@@ -1,5 +1,6 @@
 package ch07.register;
 
+import ch07.exception.WeakPasswordException;
 import ch07.passwordchecker.WeakPasswordChecker;
 
 public class UserRegister {
@@ -10,6 +11,8 @@ public class UserRegister {
     }
 
     public void register(String id, String pw, String email) {
-
+        if(passwordChecker.checkPasswordWeak(pw)) {
+            throw new WeakPasswordException();
+        }
     }
 }
