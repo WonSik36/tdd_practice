a8e7ea3e1ed2f4738c3462c67b25078095af50ec
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-10 12:31:53 +0900

    ArgumentMatcher 복수개 사용

--- src/test/java/ch07/GameGenMockTest.java
+++ src/test/java/ch07/GameGenMockTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.mock;
@@ -66,5 +66,15 @@ public class GameGenMockTest {
 
         String num2 = genMock.generate(GameLevel.MID);
         assertEquals("456", num2);
+    }
+
+    @Test
+    void mixAnyAndEq() {
+        List<String> mockList = mock(List.class);
+
+        given(mockList.set(anyInt(), eq("123"))).willReturn("456");
+
+        String old = mockList.set(5, "123");
+        assertEquals("456", old);
     }
 }
