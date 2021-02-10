b41eaf50a6cc3bd6f3347ef74aa4dd65e9e872ea
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-10 12:59:17 +0900

    스텁과 스파이 대신 목객체 적용

---
+++ src/test/java/ch07/UserRegisterMockTest.java
@@ -0,0 +1,83 @@
+package ch07;
+
+import ch07.exception.DupIdException;
+import ch07.exception.WeakPasswordException;
+import ch07.notifier.EmailNotifier;
+import ch07.passwordchecker.WeakPasswordChecker;
+import ch07.register.UserRegister;
+import ch07.repository.MemoryUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+
+public class UserRegisterMockTest {
+    private UserRegister userRegister;
+    private WeakPasswordChecker mockPasswordChecker = mock(WeakPasswordChecker.class);
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+    private EmailNotifier mockEmailNotifier = mock(EmailNotifier.class);
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(mockPasswordChecker, fakeRepository, mockEmailNotifier);
+    }
+
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword() {
+        given(mockPasswordChecker.checkPasswordWeak("pw"))
+                .willReturn(true);
+
+        assertThrows(WeakPasswordException.class, () -> {
+           userRegister.register("id", "pw", "email");
+        });
+    }
+
+    @DisplayName("이미 같은 ID가 존재하면 가입 실패")
+    @Test
+    void dupIdExists() {
+        fakeRepository.save(new User("id", "pw1", "email@email.com"));
+
+        assertThrows(DupIdException.class, () -> {
+           userRegister.register("id", "pw2", "email");
+        });
+    }
+
+    @DisplayName("같은 ID가 없으면 가입 성공함")
+    @Test
+    void noDupId_RegisterSuccess() {
+        userRegister.register("id", "pw", "email");
+
+        User savedUser = fakeRepository.findById("id");
+        assertEquals("id", savedUser.getId());
+        assertEquals("email", savedUser.getEmail());
+    }
+
+    @DisplayName("가입하면 메일을 전송함")
+    @Test
+    void whenRegisterThenSendMail() {
+        userRegister.register("id", "pw", "email@email.com");
+
+        then(mockEmailNotifier)
+                .should(only()).sendRegisterEmail("email@email.com");
+    }
+
+    @DisplayName("Argument Captor 테스트")
+    @Test
+    void argumentCaptorTest() {
+        userRegister.register("id", "pw", "email@email.com");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        then(mockEmailNotifier)
+                .should().sendRegisterEmail(captor.capture());
+
+        String realEmail = captor.getValue();
+        assertEquals("email@email.com", realEmail);
+    }
+}
