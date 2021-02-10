95a723dd18da16993e78acb932eb46b04e592290
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-02-10 11:35:20 +0900

    목 객체: throw 사용

--- src/test/java/ch07/GameGenMockTest.java
+++ src/test/java/ch07/GameGenMockTest.java
@@ -3,6 +3,7 @@ package ch07;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -14,5 +15,15 @@ public class GameGenMockTest {
 
         String num = genMock.generate(GameLevel.LOW);
         assertEquals("123", num);
+    }
+
+    @Test
+    void mockThrowTest() {
+        GameNumGen genMock = mock(GameNumGen.class);
+        given(genMock.generate(null)).willThrow(IllegalArgumentException.class);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            genMock.generate(null);
+        });
     }
 }
